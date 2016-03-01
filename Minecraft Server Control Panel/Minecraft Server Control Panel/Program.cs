6d6ddab158919658ffcd42fc00cfd0c645c83af9
Siketyan <siketyan@sikeserver.com>
2016-03-01 18:46:20 +0900

    最初のコミット

---
+++ Minecraft Server Control Panel/Minecraft Server Control Panel/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Windows.Forms;
+
+namespace Minecraft_Server_Control_Panel
+{
+    static class Program
+    {
+        /// <summary>
+        /// アプリケーションのメイン エントリ ポイントです。
+        /// </summary>
+        [STAThread]
+        static void Main()
+        {
+            Application.EnableVisualStyles();
+            Application.SetCompatibleTextRenderingDefault(false);
+            Application.Run(new Form1());
+        }
+    }
+}
